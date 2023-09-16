153732298093b64f440e5c1de0c5beeeb86df8e1
Drake3241 <100158202+Drake3241@users.noreply.github.com>
2023-09-16 12:02:13 +0200

    Update whilekalk.py

--- whilekalk.py
+++ whilekalk.py
@@ -1,56 +1,63 @@
 #Kalkulator do podstawowych obliczen z wykorzystaniem pętli while true
-
 #Zaciagniecie biblioteki matematycznej żeby była możliwosc pierwiastkowania.
 import math 
 
-print('********Witaj na swoim Pythonowym Kalkulatorze********')
+print('******** Witaj na swoim Pythonowym Kalkulatorze ********')
 print(' ')
-print('Dozwolone znaki: ')
-print('Dodawanie to " + " ')
-print('Odejmowanie to " - " ')
-print('Mnożenie to " * " ')
-print('Dzielenie to " / " ')
-print('Potegowanie to " ** " ')
-print('Pierwiastkowanie to "sqrt"')
 print(' ')
-print('****Zaczynamy****')
+print('        Dozwolone znaki        ')
+print(' ')
+print('*******************************')
+print('* Dodawanie to " + "          *')
+print('* Odejmowanie to " - "        *')
+print('* Mnożenie to " * "           *')
+print('* Dzielenie to " / "          *')
+print('* Potegowanie to " ** "       *')
+print('* Pierwiastkowanie to "sqrt"  *')
+print('*******************************')
+print(' ')
+print('**** Zaczynamy ****')
 print(' ')
 
 while True:
    
     znak = input('Podaj znak działania jakiego bedziesz wykonywac : ')
 
-    if znak == 'sqrt':
-        c = float(input('Wprowadz liczbe: ')) 
-    else:
-        a = float(input('Wprowadz 1 liczbe: '))
-
-        b = float(input('Wprowadz 2 liczbe: ')) 
-
     #Liczby są zrobione przy użyciu float, żeby móc robić obliczenia z liczbami po przecinku. Jeśli nie ma takiej potrzeby można użyć int zamiast float.
 
     #Dodawanie
     if znak == '+':
+        a = float(input('Wprowadz 1 liczbe: '))
+        b = float(input('Wprowadz 2 liczbe: '))
         print(a + b)
     #Odejmowanie
     elif znak == '-':
+        a = float(input('Wprowadz 1 liczbe: '))
+        b = float(input('Wprowadz 2 liczbe: '))
         print(a - b)
     #Mnożenie
     elif znak == '*':
+        a = float(input('Wprowadz 1 liczbe: '))
+        b = float(input('Wprowadz 2 liczbe: '))
         print(a * b)
     #Dzielenie
     elif znak == '/':
+        a = float(input('Wprowadz 1 liczbe: '))
+        b = float(input('Wprowadz 2 liczbe: '))
         if b != 0:
             print(a/b)
         else:    
             print("Nie można dzielić przez zero.")
     #Potęgowanie
     elif znak == '**':
+        a = float(input('Wprowadz 1 liczbe: '))
+        b = float(input('Wprowadz 2 liczbe: '))
         print(a ** b)
     #Pierwiastkowanie
     elif znak == 'sqrt':
-        if a >= 0:
-            print(math.sqrt(a))
+        c = float(input('Wprowadz liczbe: '))
+        if c >= 0:
+            print(math.sqrt(c))
         else:
             print('Nie można obliczyć pierwiastka z liczby ujemnej.')
     #Błedny znak
@@ -59,10 +66,10 @@ while True:
     
     #Możliwość ponownego policzenia bez ponownego uruchomienia
     while True:
-        once_again = input('Czy chcesz ponownie obliczyć jakąś liczbe? (y/n): ')
-        if once_again.lower() == 'y':
+        once_again = input('Czy chcesz ponownie obliczyć jakąś liczbe? (yes/no): ')
+        if once_again.lower() == 'y' or 'yes':
             break #Ponownie załaduje kalkulator
-        elif once_again.lower() == 'n':
+        elif once_again.lower() == 'n' or 'no':
             exit() #zakonczenie programu
         else:
             print('Nie prawidłowy znak.')
